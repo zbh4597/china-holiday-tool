764b053e52973b1f3a0ac9cbd6cce332f7959fd9
zhangbh <zhangbh33@gmail.com>
2021-10-24 12:36:29 +0800

    f

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function reqListener() {
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
-oReq.open("GET", "/china-holidays-" + year + ".json");
+oReq.open("GET", "china-holidays-" + year + ".json");
 oReq.send();
